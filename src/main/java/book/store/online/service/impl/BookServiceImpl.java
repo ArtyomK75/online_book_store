f0314032dbcbb632d92d66a41dd2c17f97a8d1fe
Artem Kostenko <a_kostenco@yahoo.com>
2023-08-27 11:57:27 +0300

    add BookController, changes in BookRepository, BookService

--- src/main/java/book/store/online/service/impl/BookServiceImpl.java
+++ src/main/java/book/store/online/service/impl/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package book.store.online.service.impl;
 
+import book.store.online.exception.EntityNotFoundException;
 import book.store.online.model.Book;
 import book.store.online.repository.BookRepository;
 import book.store.online.service.BookService;
@@ -20,7 +21,13 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List findAll() {
+    public List<Book> findAll() {
         return bookRepository.findAll();
+    }
+
+    @Override
+    public Book getById(Long id) {
+        return bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book by id: " + id));
     }
 }
