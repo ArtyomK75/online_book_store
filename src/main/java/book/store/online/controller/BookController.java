52fd746673b6b89e126b71543f22c3868aec075f
Artem Kostenko <a_kostenco@yahoo.com>
2023-09-02 10:28:52 +0300

    add LiquiBase, SoftDelete, CriteriaQuery.

--- src/main/java/book/store/online/controller/BookController.java
+++ src/main/java/book/store/online/controller/BookController.java
@@ -6,6 +6,7 @@ import book.store.online.dto.response.BookDto;
 import book.store.online.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -42,6 +44,7 @@ public class BookController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBook(@PathVariable Long id) {
         bookService.delete(id);
     }
