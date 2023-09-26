0b490e6f42871c2f3c13637f44a93a47217baed4
Artem Kostenko <a_kostenco@yahoo.com>
2023-09-26 09:22:19 +0300

    add tests

--- src/main/java/book/store/online/OnlineBookStoreApplication.java
+++ src/main/java/book/store/online/OnlineBookStoreApplication.java
@@ -6,12 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-    private final BookService bookService;
-
-    public OnlineBookStoreApplication(BookService bookService) {
-        this.bookService = bookService;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
     }
